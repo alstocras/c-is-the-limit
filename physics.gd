69d299a73dd86943ab07dd0731b63c97872b922c
Alstocras <alstoperiment@gmail.com>
2026-04-02 16:22:01 +0000

    fixed the clamping bug
    WHY GODOT? WHY? EVER HEARD OF DIFFERENTIALS AND INTEGRALS? NO BECAUSE— wait ill just add them myself — just kidding, im not doing that

--- physics.gd
+++ physics.gd
@@ -3,5 +3,5 @@ extends Node;
 const SPEED_OF_LIGHT_IN_HERTZ_METRES: float = 299_792_458.0;
 
 func getLorentzFactorFromVelocityInHertzMetres(velocity: float) -> float:
-	var safeVelocity: float = clamp(velocity, 0, SPEED_OF_LIGHT_IN_HERTZ_METRES - 1e-9);
+	var safeVelocity: float = clamp(velocity, 0, SPEED_OF_LIGHT_IN_HERTZ_METRES - 1e-5);
 	return (sqrt(1 - (safeVelocity ** 2) / (SPEED_OF_LIGHT_IN_HERTZ_METRES ** 2))) ** -1;
